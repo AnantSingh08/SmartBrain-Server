60db643e17b5a43dd4e3d057682928963ffad3ed
AnantSingh08 <anant.comics2000@gmail.com>
2022-05-18 23:45:12 +0530

    code review

--- node/server.js
+++ node/server.js
@@ -24,12 +24,12 @@ app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 app.get('/', (req, res)=> { res.send('it is working!') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', (req, res) => {signin.handleSignin(req,res,db, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT||3000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
 })
